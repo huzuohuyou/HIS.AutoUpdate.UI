680a3731bf4600fb2ca6eec27d46e257cfff94d5
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2021-02-25 09:48:00 +0800

    auto upgrade 2.0

---
+++ Models/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIS.AutoUpdate.Blazor.Models
+{
+    public class Service
+    {
+
+        public Service(string name) {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public List<Site> Columns { get; set; } = new List<Site>();
+    }
+}
